c0c561e0c4d19e58a7d6d85aaa346d7b7152e947
FW661 <frankwong661@gmail.com>
2021-12-09 15:30:22 -0400

    add routing for 3 main screens

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
+
 from kivy.app import App
+from kivy.uix import widget
+from kivymd.app import MDApp
 from kivy.uix.widget import Widget
+from kivy.lang import Builder, builder
+from kivy.uix import button
+from kivy.properties import NumericProperty, StringProperty, BooleanProperty,\
+    ListProperty, ObjectProperty 
+from kivy.uix.screenmanager import CardTransition, FadeTransition, FallOutTransition, NoTransition, RiseInTransition, ScreenManager, Screen, SlideTransition, SwapTransition, WipeTransition
+from kivymd.uix.button import MDRectangleFlatButton
 
 
-class ScreenManager(Widget):
+class ToolBar(Screen):
     pass
-class MentalMendingApp(App):
+class Form(Screen):
+    pass
+class MainHub(Screen):
+    pass
+class BossHub(Screen):
+    pass
+
+
+class MentalMendingApp(MDApp):
+  
     def build(self):
-        return ScreenManager()
+        self.theme_cls.theme_style = "Light" # "Dark"
+        self.theme_cls.primary_palette = "LightBlue"
+        self.theme_cls.primary_hue = '500'
+        sm = ScreenManager()
+        sm = ScreenManager(transition=NoTransition())
+        sm.add_widget(Form(name='form'))
+        sm.add_widget(MainHub(name='mainhub'))
+        sm.add_widget(BossHub(name='bosshub'))
+        return sm
+
+    def load_page(self, screen_name):
+        self.root.current = screen_name
+
+    #   screen = Builder.load_string(screen_toolBar)
+   
 
 if __name__ == '__main__':
     MentalMendingApp().run()
--- micellaneous/README.md
+++ micellaneous/README.md
@@ -7,7 +7,11 @@ The app must have, as a bare minimun:
 - routing: create new battle, main battle hub, and the battle scenario
 - create new battle: name of the enemy and the emotion asociated
 - main battle hub: icons to select and resume current battles
-- battle scenario: player as a trail, enemy in the top with his idle animation, damage feedbacj, and attack of the enemy
+- battle scenario: player as a trail, enemy in the top with his idle animation, damage feedback, and attack of the enemy
 - logo and stable deploy
 
 2 days to develop each point
+
+note for develop:
+all the proyect is build on top of mentalmending.kv, our principal ui template, in order to convert every aspect of the app in modules, when need to specify two main widgets, the first one is going to be the top and bottom addon, which one version will be for menu navigation, and the other version to the battle menu; the second main widget is going to be the current screen, and this will be the forman, main hub or the batlle place
+this ensures that we can referenciate those widgets allways loaded in the main kv
