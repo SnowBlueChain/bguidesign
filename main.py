129cdb7a980f5364843b6fd7d429340e430ff876
FW661 <frankwong661@gmail.com>
2021-12-12 19:11:46 -0400

    add activeBattle dictionary list

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from kivy.uix.spinner import Spinner
 
 LabelBase.register(name='pixel', fn_regular='micellaneous/pixel.ttf')
 
+activeBattles = []
+newBattle = {}
 class ToolBar(Screen):
     pass
 class Form(Screen):
@@ -16,24 +18,26 @@ class MainHub(Screen):
     pass
 class BossHub(Screen):
     pass
-# class MySpinnerOption(SpinnerOption):
-#     pass
 class FormBase(Widget):
     name_input = ObjectProperty(None)
     dropDown_button = ObjectProperty(None)
+    emotionSelector = ObjectProperty(None)
     
     def name_selection(self):
-        print(self.name_input.text)
-    def dropDown(self):
-        print('hola')
-    pass
+        global newBattle
+        newBattle['enemyName'] = self.name_input.text
+        self.name_input.text = ''
+        self.emotionSelector.emotion_selection()
+        self.emotionSelector.text = 'Pick an emotion'
+        print(self.name_input.text, newBattle)
+
+
 class EmotionPicker(Spinner):
 
     def emotion_selection(self):
-        print(self.text)
-
-    pass
-
+        global newBattle
+        newBattle['emotion'] = self.text
+        print(self.text, newBattle)
 
 class MainHubBase(Widget):
     pass
@@ -48,13 +52,18 @@ class MentalMendingApp(App):
         sm.add_widget(Form(name='form'))
         sm.add_widget(MainHub(name='mainhub'))
         sm.add_widget(BossHub(name='bosshub'))
-
-        
-
         return sm
 
     def load_page(self, screen_name):
         self.root.current = screen_name
+
+    def new_battle(self):
+        global activeBattles
+        global newBattle
+        activeBattles.append(newBattle)
+        newBattle = {}
+        self.load_page('mainhub')
+        print(activeBattles)
 
     #   screen = Builder.load_string(screen_toolBar)
    
